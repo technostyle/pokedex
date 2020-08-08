aad2655c7b5ae36e3f0e13fd4cebee8a776a499c
danila <dm.perlov@jet.su>
2020-08-08 13:29:15 +0300

    additional card info stat

--- app/pokelist/pokecard.jsx
+++ app/pokelist/pokecard.jsx
@@ -2,10 +2,8 @@ import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
 import CardActionArea from '@material-ui/core/CardActionArea'
-import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
-import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 import Chip from '@material-ui/core/Chip'
 
@@ -18,7 +16,7 @@ const useStyles = makeStyles({
   }
 })
 
-export const Pokecard = ({ name, avatar, types, onCardClick }) => {
+export const Pokecard = ({ name, avatar, types, height, weight, onCardClick }) => {
   const classes = useStyles()
   const onClick = () => onCardClick(name)
   return (
@@ -38,17 +36,10 @@ export const Pokecard = ({ name, avatar, types, onCardClick }) => {
             {types.map(type => <Chip key={type} variant="outlined" color="primary" label={type}/>)}
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
+            {`height: ${height} weight: ${weight}`}
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
     </Card>
   )
 }
--- app/pokelist/pokelist.jsx
+++ app/pokelist/pokelist.jsx
@@ -18,9 +18,9 @@ const FormRow = ({ row, onCardClick }) => {
 
   return (
     <React.Fragment>
-      {row.map(({ name, avatar, types }, idx) => (
+      {row.map(({ name, avatar, types, height, weight }, idx) => (
         <Grid key={idx} item xs={4}>
-          <Pokecard name={name} avatar={avatar} types={types} onCardClick={onCardClick}/>
+          <Pokecard name={name} avatar={avatar} types={types} height={height} weight={weight} onCardClick={onCardClick}/>
         </Grid>
       ))}
     </React.Fragment>
