16b10503e908d4bbb34bfd430c3694b56c2c1f4e
danila <dm.perlov@jet.su>
2020-08-10 09:51:00 +0300

    info modal title alignment

--- app/containers/pokelist/info-dialog.jsx
+++ app/containers/pokelist/info-dialog.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import DialogTitle from '@material-ui/core/DialogTitle'
 import Dialog from '@material-ui/core/Dialog'
+import { makeStyles } from '@material-ui/core/styles'
 import { observer } from 'mobx-react-lite'
 import { useStore } from 'store'
 import { flow } from 'lodash'
@@ -8,7 +9,12 @@ import { equals, prop } from '../../utils'
 import { InfoTable } from './info-dialog-table'
 import { AbilitiesList } from './abilities-list'
 
+const useStyles = makeStyles({
+  title: { marginLeft: '-10px' }
+})
+
 export const InfoDialog = observer(({ open, onClose, pokemonName }) => {
+  const classes = useStyles()
   const store = useStore()
   const pokelist = store.pokelist
   if (!pokelist) {
@@ -19,7 +25,7 @@ export const InfoDialog = observer(({ open, onClose, pokemonName }) => {
   return (
     <div>
       <Dialog onClose={onClose} aria-labelledby="simple-dialog-title" open={open}>
-        <DialogTitle id="simple-dialog-title">{pokemonName}</DialogTitle>
+        <DialogTitle className={classes.title} id="simple-dialog-title">{pokemonName}</DialogTitle>
         {pokemon && <AbilitiesList pokemon={pokemon}/> }
         {pokemon && <InfoTable pokemon={pokemon}/>}
       </Dialog>
